73b33bbc1fff908b3c108c5989c9f5dc2020facf
1gnaci0 <otrosnombresfrancisco@gmail.com>
2023-03-08 21:33:47 -0300

    Codigo final

--- Ejercicio21.java
+++ Ejercicio21.java
@@ -5,98 +5,113 @@
  */
 package java_ejercicios_18al21;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Usuario
  */
 public class Ejercicio21 {
 
-    public static void main(String[] args) {
-        System.out.println("--------");
-        int[][] m = mm();
-        int[][] p = pp();
-        int[] v = vv(m, p);
-
-        for (int i = 0; i < v.length; i++) {
-            System.out.print(v[i] + " | ");
-
-        }
-
-    }
-    
-/////////////////// relleno y mostrar matriz 10 x 10 //////////////////
-    public static int[][] mm() {
+     public static void main(String[] args) {
+        int[][] m = new int[10][10];
+        int[][] p = new int[3][3];
+        int[] v = new int[9];
+        String [] v_row = new String [9];
+        int [] v_colum = new int [9];
         
-        int[][] mm = new int[10][10];
+        Scanner leer = new Scanner(System.in);
 
-        for (int i = 0; i < mm.length; i++) {
-            for (int j = 0; j < mm.length; j++) {
-                mm[i][j] = (int) (Math.random() * 2);
-            }
-        }
+        for (int i = 0; i <= m.length - 1; i++) {
+            for (int j = 0; j <= m.length - 1; j++) {
 
-        for (int i = 0; i < mm.length; i++) {
-            for (int j = 0; j < mm.length; j++) {
-                System.out.print(mm[i][j]);
+                m[i][j] = (int) (Math.random() * 9);
+                System.out.print(m[i][j] + " | ");
+
             }
             System.out.println("");
         }
-        
-        System.out.println("");
-        return mm;
-    }
-    
-//////////////// relleno y mostrar matriz 3 x 3 ///////////////
-    public static int[][] pp() {
-
-        int[][] pp = new int[3][3];
-
-        for (int i = 0; i < pp.length; i++) {
-            for (int j = 0; j < pp.length; j++) {
-                pp[i][j] = 1;
-            }
-        }
-
-        for (int i = 0; i < pp.length; i++) {
-            for (int j = 0; j < pp.length; j++) {
-                System.out.print(pp[i][j]);                
-            }
-            System.out.println("");
-        }
-        
-        System.out.println("");
-        return pp;
-    }
-    
-/////////// valor vecctor /////////////////
-    public static int[] vv(int[][] mm, int[][] pp) {
-        int[] vv = new int[10];
         int cont = 0;
-        int cont2 = 0;
-        int cont3 = 0;
+        
+        do {
+            System.out.println("Ingresa 9 numeros para la matriz:");
+            //int num = leer.nextInt();
 
-        for (int i = 0; i < mm.length; i++) {
-            for (int j = 0; j < mm.length; j++) {
-                if (pp[cont][cont2] == mm[i][cont3]) {
-                    vv[i] = mm[i][cont3];
-                } else {
-                    vv[i] = -1;
-                }
-                cont2 = cont2 + 1;
-                cont3 = cont3 + 1;
-                if (cont2 == 3) {
-                    cont = cont + 1;
-                    cont2 = 0;
-                }
-                if (cont == 3) {
-                    cont = 0;
-                }
-                if (cont3 == 10) {
-                    cont3 = 0;
-                }
+            for (int i = 0; i <  9; i++) {
+                int num = leer.nextInt();
+                v[i] = num;
+
+                cont = cont + 1;
             }
+        } while (cont == 8);
+        
+        for (int i = 0; i < 9; i++) {
+            System.out.print(v[i]+"-");
+        }
+        System.out.println("");
+        int sum = 0;
+        for (int i = 0; i < p.length ; i++) {
+            for (int j = 0; j < p.length ; j++) {
+                p[i][j] = v[sum];
+                sum = sum + 1;
+            }
+           
+        }
+
+        for (int i = 0; i < p.length; i++) {
+            for (int j = 0; j < p.length; j++) {
+                System.out.print(p[i][j]+" | ");
+                
+            }
+            System.out.println("");
         }
         
-        return vv;
+        
+        
+        boolean found = false;
+        int row = -1, col = -1;
+        int n = 0;
+        
+        
+        for (int i = 0; i <= m.length - p.length; i++) {
+            for (int j = 0; j <= m[i].length - p[0].length; j++) {
+                boolean match = true;
+           
+                for (int k = 0; k < p.length; k++) {
+                    for (int l = 0; l < p[k].length; l++) {
+                        if (p[k][l] != m[i + k][j + l]) {
+                            match = false;
+                            break;
+                        }else{
+                        
+                        v_row[n] = (String.valueOf(k)+" "+String.valueOf(l));
+                        n = n + 1;
+                       
+                        }
+                    }
+                    if (!match) {
+                        break;
+                    }
+                }
+                if (match) {
+                    found = true;
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+            if (found) {
+                break;
+            }
+        }
+
+        if (found) {
+            System.out.println("La submatriz P se encuentra en la matriz M en la fila " + row + " y la columna " + col);
+        } else {
+            System.out.println("La submatriz P no se encuentra en la matriz M");
+        }
+        for (int i = 0; i < v_row.length-1; i++) {
+            System.out.print(v_row[i]+" | ");
+        }
     }
 }
